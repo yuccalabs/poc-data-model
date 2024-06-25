6f158fc66aa124a2db7ca10befe025bbdb819b92
Ahmed Siouani <ahmed.siouani@gmail.com>
2024-06-25 11:38:40 +0000

    Scripts for approach 1 added

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # poc-data-model
+
+
+### Schémas de données:
+- Approche 1: Modèle de donnée Saas multi-table [company_prefix]__table_name (exemple de la table employee)
+- Approche 2: Modèle de donnée Saas mono-table table_name (exemple de la table employée)
+
+### Jeux de données:
+- Approche 1:  5000 tables [company_prefix]_employees (5000 clients) avec 100.000 lignes par tables
+- Approche 2: Une table employee avec 500000000 entrée
+
+### The implementation:
+
+>tables.sql contains scripts for approach 1
+ 
+> table.sql contains scripts for approach 2
+
+### Execution:
+
+> CREATE DATABASE MY_DATABASE;
+
+>
+> 
+> 
---
+++ tables.sql
@@ -0,0 +1,24 @@
+do $$
+    DECLARE nbr_of_tables integer;
+    DECLARE nbr_of_lines integer;
+begin
+    for nbr_of_tables in 1..5000 loop
+        EXECUTE format('
+            CREATE TABLE
+                employee_%s (
+                    id INT NOT NULL,
+                    firstname VARCHAR(255) NOT NULL,
+                    lastname VARCHAR(255) NOT NULL,
+                    phone_number VARCHAR(20) NOT NULL,
+                    company VARCHAR(255) NOT NULL
+                 )
+            ;
+        ', nbr_of_tables);
+    end loop;
+
+    for nbr_of_tables in 1..5000 loop
+        for nbr_of_lines in 1..100000 loop
+          EXECUTE format('INSERT INTO %s VALUES ($1,$2,$3,$4)'::text ,nbr_of_tables) using nbr_of_lines, 'john', 'doe', '0666666666', 'company';
+        end loop;
+    end loop;
+end; $$
